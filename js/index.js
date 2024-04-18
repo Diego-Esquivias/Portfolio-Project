24a07a0f7654e8658747d6369fe377621705b9f9
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-04-18 12:05:51 -0700

    Update
    - added keyframe animations
    - added a new way to show my work

--- js/index.js
+++ js/index.js
@@ -5,21 +5,48 @@ $(function () {
     //         $('.homeContent').slideUp(i)
     //     }
     // })
-    $('.resumeGrid').animate({
-        height: '1200px'
-    }, 'slow')
+    // $('.resumeGrid').animate({
+    //     height: '1200px'
+    // }, 'slow')
 
-    setTimeout(slide, 500)
+    // setTimeout(slide, 500)
     
-    function slide(){
-        $('#item-1, #item-2, #item-3, #item-4, #item-5, #item-6, #item-7, #item-8').slideDown(2500)
+    // function slide(){
+    //     $('#item-1, #item-2, #item-3, #item-4, #item-5, #item-6, #item-7, #item-8').slideDown(2500)
+    // }
 
-        $('#nameTitle').click(function () {
-            $('#name').slideToggle(1000)
-        })
+    $('#nameTitle').hover(function () {
+        $('#name').slideToggle(1000)
+    })
+    
+    $('#hobbyTitle').hover(function () {
+        $('#hobby').slideToggle(1000)
+    })
 
-        $('#hobbyTitle').click(function () {
-            $('#hobby').slideToggle(1000)
-        })
-    }
 })
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
