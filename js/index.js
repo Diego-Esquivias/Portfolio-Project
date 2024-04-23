1a4ca703f227be3c587ed89aa155c514da80e7a0
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-04-22 18:18:06 -0700

    Update
    - Cleaned up code a bit
    - Added responsiveness
    - added some comments

--- js/index.js
+++ js/index.js
@@ -1,20 +1,4 @@
 $(function () {
-    // $('.homeContent').hover(function {
-    //     for (let i = 5000; i == 1000; i -= 1000) {
-    //         $('.homeContent').slideDown(i)
-    //         $('.homeContent').slideUp(i)
-    //     }
-    // })
-    // $('.resumeGrid').animate({
-    //     height: '1200px'
-    // }, 'slow')
-
-    // setTimeout(slide, 500)
-    
-    // function slide(){
-    //     $('#item-1, #item-2, #item-3, #item-4, #item-5, #item-6, #item-7, #item-8').slideDown(2500)
-    // }
-
     $('#nameTitle').click(function () {
         $('#name').slideToggle(1000)
     })
